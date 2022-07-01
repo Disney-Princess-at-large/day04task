2fd0a84fc754198aa7da753ff40a36df41fecda0
grj <3303272844@qq.com>
2022-07-01 19:41:06 +0800

    vue侦听器的练习

---
+++ src/components/06.vue侦听器.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="text" v-model.lazy='name'>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+  return {
+    name:'123'
+  }
+},
+// 目标: 侦听到name值的改变
+  /*
+  语法:
+    watch: {
+      变量名 (newVal, oldVal){
+        // 变量名对应值改变这里自动触发
+        // newVal 是改变之后的值
+        // oldVal 是改变之前的值  
+      }
+    }
+  */
+watch:{
+  name(newVal,oldVal){
+  // newVal: 当前最新值
+  // oldVal: 上一刻值
+    console.log(newVal,oldVal);
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
