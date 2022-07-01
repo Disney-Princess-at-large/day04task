fc3bdb673126afd04a4d2c2f7f955727de47379f
grj <3303272844@qq.com>
2022-07-01 17:57:52 +0800

    品牌管理案例的功能完善

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue"; //引入vue
+import App from "./App.vue"; //引入app.vue 应用的根组件
+import "bootstrap/dist/css/bootstrap.css"; // 默认找文件夹下的index文件(但是这个不是所以需要写路径)
+Vue.config.productionTip = false; //提示语
 
 Vue.filter("toDown", (val) => {
   return val.toLowerCase();
 });
 
-Vue.filter("toReverse", (val,split) => {
-  return val.split("").reverse().join(split || "");
+Vue.filter("toReverse", (val, split) => {
+  return val
+    .split("")
+    .reverse()
+    .join(split || "");
 });
-
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  //实例化vue对象出来
+  render: (h) => h(App), //渲染根应用
+}).$mount("#app"); //渲染出来的挂载到页面
