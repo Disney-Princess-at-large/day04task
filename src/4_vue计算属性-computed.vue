bf54732048203cd36d75ca6b91cefbde262e67d0
Hello-XY <1661230401@qq.com>
2022-07-01 21:07:22 +0800

    完成vue计算属性

---
+++ src/4_vue计算属性-computed.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    购物车商品总价：
+    <!-- 商品是否大于500 符合条件可使用400的优惠券 否则不可使用 -->
+    {{ totalPrice() }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      total: 5,
+      price: 98,
+      coupon: 400,
+    };
+  },
+
+  methods: {
+    totalPrice() {
+      return this.total * this.price >= 500
+        ? this.total * this.price - this.coupon
+        : this.total * this.price;
+    },
+  },
+};
+</script>
---
+++ src/6_vue计算属性-完整写法.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>
+      <span>姓名:</span>
+      <input type="text" v-model="full" />
+    </div>
+  </div>
+</template>
+
+<script>
+// 问题: 给计算属性赋值 - 需要setter
+// 解决:
+/*
+    完整语法:
+    computed: {
+        "计算属性名" (){},
+        "计算属性名": {
+            set(值){
+
+            },
+            get(){
+                return 值
+            }
+        }
+    }
+*/
+export default {
+  computed: {
+    full: {
+      set(val) {
+        console.log(val);
+      },
+      get() {
+        return "张三";
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
